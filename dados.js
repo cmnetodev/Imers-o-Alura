248dbab550dce1f20a5304e2056fd65a52b95d6a
Celso Moreira <cmneto.dev@gmail.com>
2024-09-07 00:50:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma das maiores promessas do skate mundial. Com apenas [idade], a atleta já conquistou diversos títulos e se tornou um ícone da juventude. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que conquistou o país com sua força e determinação. A atleta, nascida em [cidade], se destacou nas competições internacionais e se tornou um exemplo para as novas gerações de judocas.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é um nome que brilha no cenário da ginástica artística mundial. Nascida em Guarulhos, a atleta conquistou o coração dos brasileiros com sua determinação e talento. Sua trajetória é marcada por superação, dedicação e muito suor, o que a tornou um símbolo do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
